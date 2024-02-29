48f46fdc2163dda58ef5f62bc3f83937982dba6a
Vinícius Hoyer <contact@vhoyer.dev>
2024-02-29 15:50:00 -0300

    :lipstick: Add title to navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,23 +2,38 @@
 
 <template>
   <div class="container">
-    <header>
-      <nav class="nav">
-        <NuxtLink to="/">
-          Home
-        </NuxtLink>
-      </nav>
-    </header>
+    <div class="navbar">
+      <strong class="title-1">
+        JP点々
+      </strong>
+
+      <header>
+        <nav class="nav">
+          <NuxtLink to="/">
+            Home
+          </NuxtLink>
+        </nav>
+      </header>
+    </div>
 
     <slot />
 
-    <footer class="mt-1200 text-center">
+    <footer class="my-1200 text-center">
       made with love by veshworks &copy; {{ new Date().getFullYear() }}
     </footer>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--space-0300) 0;
+  margin-bottom: var(--space-0300);
+  border-bottom: 1px solid var(--color-100);
+}
+
 .nav {
   display: flex;
   gap: var(--space-0200);
